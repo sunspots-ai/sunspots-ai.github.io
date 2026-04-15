f0da65b015ff9dcb465792a207e6ac0ae868d73b
sunspots-ai <tamara.chuchuk@gmail.com>
2026-04-15 13:38:17 +0800

    Update main.js
    подчистка от ненужного кода

--- main.js
+++ main.js
@@ -2,9 +2,7 @@
 (function() {
   "use strict";
 
-  /**
-   * Easy selector helper function
-   */
+  /*спомогательная функция-обертка для поиска элементов на веб-странице*/
   const select = (el, all = false) => {
     el = el.trim()
     if (all) {
@@ -14,9 +12,6 @@
     }
   }
 
-  /**
-   * Easy event listener function
-   */
   const on = (type, el, listener, all = false) => {
     let selectEl = select(el, all)
 
@@ -29,9 +24,7 @@
     }
   }
 
-  /**
-   * Scrolls to an element with header offset
-   */
+  /*плавное перемещение при меню*/
   const scrollto = (el) => {
     window.scrollTo({
       top: 0,
@@ -39,18 +32,8 @@
     })
   }
 
-  /**
-   * Mobile nav toggle
-   */
-  on('click', '.mobile-nav-toggle', function(e) {
-    select('#navbar').classList.toggle('navbar-mobile')
-    this.classList.toggle('bi-list')
-    this.classList.toggle('bi-x')
-  })
 
-  /**
-   * Scrool with ofset on links with a class name .scrollto
-   */
+
   on('click', '#navbar .nav-link', function(e) {
     let section = select(this.hash)
     if (section) {
@@ -73,7 +56,7 @@
         navbarToggle.classList.toggle('bi-list')
         navbarToggle.classList.toggle('bi-x')
       }
-
+/*показ секций*/
       if (this.hash == '#header') {
         header.classList.remove('header-top')
         sections.forEach((item) => {
@@ -102,9 +85,6 @@
     }
   }, true)
 
-  /**
-   * Activate/show sections on load with hash links
-   */
   window.addEventListener('load', () => {
     if (window.location.hash) {
       let initial_nav = select(window.location.hash)
@@ -132,9 +112,7 @@
     }
   });
 
-  /**
-   * Skills animation
-   */
+  /*анимации */
   let skilsContent = select('.skills-content');
   if (skilsContent) {
     new Waypoint({
@@ -149,94 +127,6 @@
     })
   }
 
-  /**
-   * Testimonials slider
-   */
-  new Swiper('.testimonials-slider', {
-    speed: 600,
-    loop: true,
-    autoplay: {
-      delay: 5000,
-      disableOnInteraction: false
-    },
-    slidesPerView: 'auto',
-    pagination: {
-      el: '.swiper-pagination',
-      type: 'bullets',
-      clickable: true
-    },
-    breakpoints: {
-      320: {
-        slidesPerView: 1,
-        spaceBetween: 20
-      },
-
-      1200: {
-        slidesPerView: 3,
-        spaceBetween: 20
-      }
-    }
-  });
-
-  /**
-   * Porfolio isotope and filter
-   */
-  window.addEventListener('load', () => {
-    let portfolioContainer = select('.portfolio-container');
-    if (portfolioContainer) {
-      let portfolioIsotope = new Isotope(portfolioContainer, {
-        itemSelector: '.portfolio-item',
-        layoutMode: 'fitRows'
-      });
-
-      let portfolioFilters = select('#portfolio-flters li', true);
-
-      on('click', '#portfolio-flters li', function(e) {
-        e.preventDefault();
-        portfolioFilters.forEach(function(el) {
-          el.classList.remove('filter-active');
-        });
-        this.classList.add('filter-active');
-
-        portfolioIsotope.arrange({
-          filter: this.getAttribute('data-filter')
-        });
-      }, true);
-    }
-
-  });
-
-  /**
-   * Initiate portfolio lightbox 
-   */
-  const portfolioLightbox = GLightbox({
-    selector: '.portfolio-lightbox'
-  });
-
-  /**
-   * Initiate portfolio details lightbox 
-   */
-  const portfolioDetailsLightbox = GLightbox({
-    selector: '.portfolio-details-lightbox',
-    width: '90%',
-    height: '90vh'
-  });
-
-  /**
-   * Portfolio details slider
-   */
-  new Swiper('.portfolio-details-slider', {
-    speed: 400,
-    loop: true,
-    autoplay: {
-      delay: 5000,
-      disableOnInteraction: false
-    },
-    pagination: {
-      el: '.swiper-pagination',
-      type: 'bullets',
-      clickable: true
-    }
-  });
+ 
 
 })()
